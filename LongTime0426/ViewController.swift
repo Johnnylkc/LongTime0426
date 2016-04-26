8ad2e12881db549b897c995fdc0d831f98bdb601
Johnnylkc <momozzz1231@gmail.com>
2016-04-26 19:47:26 +0800

    Initial Commit

---
+++ LongTime0426/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LongTime0426
+//
+//  Created by 劉坤昶 on 2016/4/26.
+//  Copyright © 2016年 JohnnyKetchup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
